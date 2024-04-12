cd9d083879843fe73e3c721796b513294b5da39f
wo55along <91838584+wo55along@users.noreply.github.com>
2024-04-12 14:32:46 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! [![github]](https://github.com/dtolnay/enumn)&ensp;[![crates-io]](https://crates.io/crates/enumn)&ensp;[![docs-rs]](https://docs.rs/enumn)
+//! [![github]](https://github.com/wo55along)&ensp;[![crates-io]](https://crates.io/crates/enumn)&ensp;[![docs-rs]](https://docs.rs/enumn)
 //!
 //! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
 //! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
